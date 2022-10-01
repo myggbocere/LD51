9cd07f697b0d81cb98f171a146ab3c5389c72e04
Eagan Kaminetz <eagangk@protonmail.com>
2022-09-30 20:00:28 -0700

    added dev test scene, tilemap collisions, woodwall terrain

---
+++ devstuff/dev_testscene_camera.gd
@@ -0,0 +1,19 @@
+extends Camera2D
+const ZOOM_INCREMENT_VEC = Vector2(1.2, 1.2)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if event.is_action_pressed("level_zoom_in") and (zoom*ZOOM_INCREMENT_VEC).x < 6: #use x because they'll always be the same and easier than comparing vectors
+		zoom *= ZOOM_INCREMENT_VEC
+		print(zoom)
+	elif event.is_action_pressed("level_zoom_out") and (zoom/ZOOM_INCREMENT_VEC).x > 1.2:
+		zoom /= ZOOM_INCREMENT_VEC
+		print(zoom)
